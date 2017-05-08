138f4739988de4e1493e8070c5bbdb7bff2ffb4d
Manas Mishra <manas@7halo.in>
2017-05-08 21:26:45 +0530

    Initial Commit

---
+++ Friest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Friest
+//
+//  Created by Nauroo on 08/05/17.
+//  Copyright © 2017 Manas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
